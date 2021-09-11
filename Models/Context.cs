1691e141b9823c666d3df9dc3199124c5bc89b11
afnnunes <afn.nunes@gmail.com>
2021-09-11 16:45:04 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CursoAspnetMVC.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set;}
+        public virtual DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost; Database=Cursomvc; User=SA; Password=123456");
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoAspnetMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage ="Quantidade fora da faixa permitida")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        [Required(ErrorMessage = "O campo categoria é obrigatório")]
+        public Categoria Categoria { get; set; }
+
+    }
+}
